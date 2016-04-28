65c81a1343f111b7a7d325649d0650e2545645c4
CIS 550 <cis550@seas.upenn.edu>
2016-04-28 16:01:06 +0100

    Converted Linker into module with async function

--- datalake_mgmt_system/linker.js
+++ datalake_mgmt_system/linker.js
@@ -1,23 +1,16 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('localhost:27017/test');
 var cosine = require('cosine');
 var stopwords = require('stopwords').english;
 
 var keyValueCollection = db.get('keyValueCollection');
 var linkCollection = db.get('linkCollection');
 
-//Example Initialization. Remove Later
-keyValueCollection.remove();
-linkCollection.remove();
-keyValueCollection.insert({key : "key1", value: "abc", source:"myfile.pdf"},null);
-keyValueCollection.insert({key : "key2", value: "myfile.txt", source:"myfile.xml"},null);
-keyValueCollection.insert({key : "key3", value: "key1", source:"myfile.txt"},null);
-
-var promise = keyValueCollection.find().each( function(keyValue1) { 
-            keyValueCollection.find().each( function(keyValue2) 
+exports.async_linker = function(keyValueCollection, linkCollection, callback){
+	var dbReadPromise = keyValueCollection.find().each( function(keyValue1) { 
+	    keyValueCollection.find().each( function(keyValue2) 
 				{ 
-					//console.log( keyValue1.key + "_" + keyValue1.value + "_" + keyValue1.source + " x " + keyValue1.key + "_" + keyValue2.value + "_" + keyValue2.source );
 					if(
 						//Step 0: keyValue1 !== keyValue2
 						JSON.stringify(keyValue1) !== JSON.stringify(keyValue2) &&
@@ -32,15 +25,24 @@ var promise = keyValueCollection.find().each( function(keyValue1) {
 							(cosine(keyValue1.value.split(/\s/).filter( function( el ) { return stopwords.indexOf( el ) < 0; } ), 
 								keyValue2.value.split(/\s/).filter( function( el ) { return stopwords.indexOf( el ) < 0; } )) >= 0.80)
 						)
-						
+					
 					  ) {
 
 						//console.log( keyValue1.key + "_" + keyValue1.value + "_" + keyValue1.source + " x " 
 								//+ keyValue1.key + "_" + keyValue2.value + "_" + keyValue2.source);
 
-						linkCollection.insert({node_id_1: keyValue1._id, node_id_2: keyValue2._id}, function (err, doc) { if (err) throw err;});
-						
+						linkCollection.insert({node_id_1: keyValue1._id, node_id_2: keyValue2._id}, 
+										function (err, doc) { if (err) throw err;});
+					
 					}
 				 } 
-			)});
+			)}).error(function (err, doc) { if (err) throw err;});
+
+	//Returns a promise that on success determines that all inserts to mongodb have been issued, but may not have been written yet
+	return dbReadPromise;
+
+};
+
+
+
 
